d21322bf8f5903d77fadeef2d591009a98e93a96
Jhonatan Luiz <jonatapqt01@gmail.com>
2022-08-12 00:53:21 -0300

    bugfix

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ class Dataset():
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
-from api.controllers.dataset import Dataset
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
