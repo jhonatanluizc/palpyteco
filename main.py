0ce19cc6cbf9f44cb3d6409861314c4ed79a7c69
Jhonatan Luiz <jonatapqt01@gmail.com>
2022-08-11 23:50:06 -0300

    bugfix

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ neuralNetwork = NeuralNetwork()
 def hello_world():
     return "hello"
 
-@app.route("/hello")
-def teste():
-    from flask import render_template
-    return render_template("hello.html")
+# @app.route("/hello")
+# def teste():
+#     from flask import render_template
+#     return render_template("hello.html")
 
-@app.get('/loaddataset')
-def load_dataset():
-    return neuralNetwork.load_dataset()
+# @app.get('/loaddataset')
+# def load_dataset():
+#     return neuralNetwork.load_dataset()
 
-@app.get('/training')
-def training():
-    return neuralNetwork.training()
+# @app.get('/training')
+# def training():
+#     return neuralNetwork.training()
 
-@app.get('/predict')
-def predict():
-    return neuralNetwork.predict()
+# @app.get('/predict')
+# def predict():
+#     return neuralNetwork.predict()
